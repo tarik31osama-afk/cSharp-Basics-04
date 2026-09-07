95e831b95cfabae77c93e12ff389afde71083d21
tarik31osama-afk <tarik31osama@gmail.com>
2026-09-07 17:37:28 +0300

    1st q

--- session04/Program.cs
+++ session04/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-          
+            #region 1st q
+            string title = "clean code";
+            string uppetTitle = title.ToUpper();
+            Console.WriteLine(title);
+            Console.WriteLine(uppetTitle);
+
+            #endregion
+
         }
     }
 }
